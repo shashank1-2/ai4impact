8db5457496d5a5fbcadf9bf4569ad27738bc8dd7
Saishashank Pallekhadi <ugs23054_cse.shashank@cbit.org.in>
2026-06-04 15:33:46 +0530

    Update API_BASE to use environment variable

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:8000';
+const API_BASE = 'import.meta.env.VITE_API_URL';
 
 function getHeaders(auth = false) {
   const headers = { 'Content-Type': 'application/json' };
